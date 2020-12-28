f3988944d8040681666f4a0cfe1cf68f3305f1e0
Damian Lesiuk <lesiuk@gmail.com>
2020-12-28 01:31:38 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿namespace AutoWard
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using LeagueSharp;
+    using LeagueSharp.Common;
+    using SharpDX;
+
+    static class Program
+    {
+        private static Menu _mainMenu;
+        private static readonly List<WardPosition> WardPositions = WardPositionReader.Read(HeroManager.Player.Team);
+        private const int WardCastDistance = 900;
+
+        private static readonly Items.Item
+            VisionWard = new Items.Item(2055, 550f);
+
+        private static readonly Items.Item
+            TrinketN = new Items.Item(3340, 600f);
+
+        private static readonly Items.Item
+            SightStone = new Items.Item(3851, 600f);
+
+        private static float _lastWardPlaced = Game.Time;
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+
+        private static void OnLoad(EventArgs args)
+        {
+            _mainMenu = new Menu("Auto Ward", "autoward", true);
+
+            _mainMenu.AddItem(new MenuItem("autoward.on", "Auto Ward").SetValue(true));
+            _mainMenu.AddItem(new MenuItem("autoward.popularity", "Min. popularity").SetValue(new Slider(40, 0, 3000)));
+            _mainMenu.AddItem(new MenuItem("autoward.score", "Min. score").SetValue(new Slider(40, 0, 600)));
+            _mainMenu.AddItem(
+                new MenuItem("autoward.on_key", "Put ward in best spot").SetValue(new KeyBind('X', KeyBindType.Press)));
+            _mainMenu.AddToMainMenu();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+            Game.OnUpdate += GameOnOnUpdate;
+        }
+
+        private static void CastWard(Vector2 position)
+        {
+            if (VisionWard.IsReady())
+            {
+                VisionWard.Cast(position);
+            }
+            else if (TrinketN.IsReady())
+            {
+                TrinketN.Cast(position);
+            }
+            else if (SightStone.IsReady())
+            {
+                SightStone.Cast(position);
+            }
+
+            _lastWardPlaced = Game.Time;
+        }
+
+        private static void GameOnOnUpdate(EventArgs args)
+        {
+            if (!_mainMenu.Item("autoward.on").GetValue<bool>() &&
+                !_mainMenu.Item("autoward.on_key").GetValue<bool>()) return;
+            if (Game.Time - _lastWardPlaced < 5) return;
+
+            var minPopularity = _mainMenu.Item("autoward.popularity").GetValue<int>();
+            var minScore = _mainMenu.Item("autoward.score").GetValue<int>();
+
+            foreach (var wardPosition in WardPositions
+                .Where(wardPosition =>
+                    wardPosition.position.Distance(HeroManager.Player.ServerPosition) < WardCastDistance &&
+                    wardPosition.popularity > minPopularity && wardPosition.score > minScore))
+            {
+                CastWard(wardPosition.position);
+                break;
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            foreach (var wardPosition in WardPositions)
+            {
+                Render.Circle.DrawCircle(wardPosition.position.To3D(), 25, System.Drawing.Color.Green, 1);
+            }
+        }
+    }
+}
---
+++ WardPositionReader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LeagueSharp;
+using SharpDX;
+
+namespace AutoWard
+{
+    public class WardPosition
+    {
+        public Vector2 position;
+        public float popularity;
+        public float score;
+    }
+
+    public static class WardPositionReader
+    {
+        public static List<WardPosition> Read(GameObjectTeam team, int minute = 0)
+        {
+            var teamString = "Chaos";
+            if (team == GameObjectTeam.Order)
+            {
+                teamString = "Order";
+            }
+
+            var output = new List<WardPosition>();
+            var content = Resource1.ResourceManager.GetString($"wards_{teamString}_All_{minute}", Resource1.Culture);
+            if (content == null) return output;
+
+            output.AddRange(content.Split('\n')
+                .Skip(1)
+                .Select(line => line.Split(','))
+                .Select(columns =>
+                    new WardPosition
+                    {
+                        position = new Vector2
+                        {
+                            X = float.Parse(columns[0]),
+                            Y = float.Parse(columns[1])
+                        },
+                        popularity = float.Parse(columns[4]),
+                        score = float.Parse(columns[5])
+                    }));
+
+            return output;
+        }
+    }
+}
